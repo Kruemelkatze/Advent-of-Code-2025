4181a78bff1a2f0cd10e65086f9dc803326e093d
Fabian Schober <fabian@dirtypaws.studio>
2025-12-01 15:28:29 +0100

    Setup

---
+++ template.ts
@@ -0,0 +1,24 @@
+import * as tk from "jsr:@es-toolkit/es-toolkit@^1.42.0";
+import { exists } from "jsr:@std/fs/exists";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Config ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const PROD = Deno.args.some((arg) => arg === "prod");
+const inputFile = PROD ? "input.txt" : "input-test.txt";
+console.log(`Using ${inputFile} (${PROD ? "PROD" : "TEST"})`);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if(!await exists(inputFile)) {
+    console.error(`Where the fuck is ${inputFile}???`);
+    Deno.exit(1);
+}
+
+const input = await Deno.readTextFile(inputFile);
+const inputLines = input.trim().split(/\r?\n/g).map((l) => l.trim());
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Here be Dragons ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+console.time("time");
+
+// CODE
+
+
+console.timeEnd("time");
